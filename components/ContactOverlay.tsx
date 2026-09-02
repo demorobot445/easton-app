e5601ea8ad084c4e3bf955d7b5587055384b645e
Ahmed <demo11website@gmail.com>
2026-09-02 18:05:33 +0500

    update underline

--- components/ContactOverlay.tsx
+++ components/ContactOverlay.tsx
@@ -161,7 +161,7 @@ export function DownloadLink({
     <button
       type="button"
       disabled={isDownloading}
-      className="text mt-4 inline-flex cursor-pointer items-center gap-2 font-medium underline disabled:cursor-default"
+      className="text mt-4 inline-flex cursor-pointer items-center gap-2 border-b-2 border-white font-medium disabled:cursor-default"
       onClick={() => handleDownload(url, filename, setIsDownloading)}
     >
       {isDownloading ? "Downloading..." : label}
